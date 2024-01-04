1c4f3ccc8fc7e6cca46a6d32512ece27acd187a4
Alex Foglia <alex.f1793@gmail.com>
2024-01-05 00:15:40 +0100

    changed default font to verdana 8 pt size

--- extras/maintenance/src/main.cpp
+++ extras/maintenance/src/main.cpp
@@ -11,6 +11,9 @@ int main(int argc, char** argv)
 	QApplication* maint;
 	maint = new QApplication(argc, argv);
 
+    QFont font("Verdana", 8);
+    qApp->setFont(font);
+
 	MaintenanceWindow* maintWindow = new MaintenanceWindow();
 	maintWindow->setVisible(true);
 
